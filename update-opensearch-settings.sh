1475051b9e24ba0092015f061f6fb975fc589141
thanachaiTP <66632967+thanachaiTP@users.noreply.github.com>
2022-06-15 10:36:25 +0700

    Delete update-opensearch-settings.sh

--- update-opensearch-settings.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -XPUT -u admin:admin -H "Content-type: application/json" 127.0.0.1:9200/_cluster/settings -d '{"persistent":{"compatibility":{"override_main_response_version":true } } }'
